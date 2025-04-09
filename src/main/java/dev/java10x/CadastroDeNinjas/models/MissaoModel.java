e370fd96c91d1a28bc32aae02ac000d094b46190
FelipeAriki <felipeariki3@gmail.com>
2025-04-09 10:50:57 -0300

    #4 - Adicionado Lombock.

--- src/main/java/dev/java10x/CadastroDeNinjas/models/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/models/MissaoModel.java
@@ -1,11 +1,17 @@
 package dev.java10x.CadastroDeNinjas.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "missao")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissaoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/dev/java10x/CadastroDeNinjas/models/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/models/NinjaModel.java
@@ -1,9 +1,15 @@
 package dev.java10x.CadastroDeNinjas.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ninja")
+@NoArgsConstructor //construtor vazio
+@AllArgsConstructor //construtor com todos atributos
+@Data //criar os Getters e Setters
 public class NinjaModel {
 
     @Id
@@ -19,36 +25,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missao_id")
     private MissaoModel missao;
-
-    public NinjaModel() {
-    }
-    public NinjaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
